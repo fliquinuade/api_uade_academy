b9d115fd68766032fdbbda7a498d2184f2a05dde
Federico Liquin <josefliquin@gmail.com>
2025-11-04 22:30:22 -0300

    modificaciones entrypoint con registro logs.

--- deployments/entrypoint.sh
+++ deployments/entrypoint.sh
@@ -22,7 +22,10 @@ if [ "$MODE" = "production" ]; then
     exec gunicorn config.wsgi:application \
         --bind 0.0.0.0:8000 \
         --workers 3 \
-        --timeout 120 
+        --timeout 120 \
+        --access-logfile - \
+        --error-logfile - \
+        --log-level info
 else
     echo "Iniciando en modo DESARROLLO con runserver..."
     exec python manage.py runserver 0.0.0.0:8000
